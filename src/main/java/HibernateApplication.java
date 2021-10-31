c2b3999695ce2f65155504b4c15dab5ae4707305
Rodion <rodionchik17@gmail.com>
2021-10-31 19:49:17 +0200

    refactoring HibernateApplication class

--- src/main/java/HibernateApplication.java
+++ src/main/java/HibernateApplication.java
@@ -8,29 +8,53 @@ import java.util.List;
 public class HibernateApplication {
     private UserEntityServiceImpl userEntityService;
 
-    UsersEntity rodion = new UsersEntity("Rodion", 26);
-    UsersEntity dmitry = new UsersEntity("Dmitry", 24);
-    AutosEntity porsche = new AutosEntity("Porsche", "Red");
-    AutosEntity lamborghini = new AutosEntity("Lamborghini", "Yellow");
-
     public HibernateApplication (UserEntityServiceImpl userEntityService) {
         this.userEntityService = userEntityService;
     }
 
     public void run () throws SQLException
     {
+        createUserAndCarsRodion();
+        createUserAndCarsDmitry();
+    }
+
+    public void createUserAndCarsRodion ()
+    {
+        UsersEntity rodion = new UsersEntity("Rodion", 26);
         userEntityService.saveUser(rodion);
-        userEntityService.saveUser(dmitry);
+
+        AutosEntity porsche = new AutosEntity("Porsche", "Red");
         porsche.setUsersEntity(rodion);
         rodion.addAuto(porsche);
 
+        AutosEntity lamborghini = new AutosEntity("Lamborghini", "Yellow");
         lamborghini.setUsersEntity(rodion);
         rodion.addAuto(lamborghini);
 
         userEntityService.updateUser(rodion);
-        userEntityService.updateUser(dmitry);
-
     }
+
+    public void createUserAndCarsDmitry ()
+    {
+        UsersEntity dmitry = new UsersEntity("Dmitry", 24);
+        userEntityService.saveUser(dmitry);
+
+        AutosEntity fiesta = new AutosEntity("Ford Fiesta", "Blue");
+        fiesta.setUsersEntity(dmitry);
+        dmitry.addAuto(fiesta);
+
+        AutosEntity granda = new AutosEntity("Ford Granda", "Silver");
+        granda.setUsersEntity(dmitry);
+        dmitry.addAuto(granda);
+
+        userEntityService.updateUser(dmitry);
+    }
+
+    public void showUsersFromDB ()
+    {}
+
+    public void showCarsFromDB ()
+    {}
 
     public void clearDB () {
         List<UsersEntity> list = userEntityService.findAllUsers();
